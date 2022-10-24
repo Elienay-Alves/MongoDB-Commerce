d9b9885421005ed9bb4194094c766684b747896a
Elienay Alves <elienay8486@gmail.com>
2022-10-24 17:20:35 -0300

    Fix: Requisito 18

--- challenges/desafio18.js
+++ challenges/desafio18.js
@@ -1,12 +1,11 @@
 db.produtos.updateMany({
   nome: {
-    $in: ["Big Mac", "Quarteirão com Queijo"]
+    $in: ["Big Mac", "Quarteirão com Queijo"],
   }, 
 }, {
     $push: {
       ingredientes: "bacon",
     },
-  },
-);
+  });
 
 db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 });
